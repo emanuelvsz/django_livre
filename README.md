4ada82edbb47e451d11f78c805b61ee17edf5bb5
emanuelvsz <usermane13@gmail.com>
2023-10-29 19:50:31 -0300

    creating project

--- README.md
+++
@@ -1,2 +0,0 @@
-# django_livre
-An API that get some information of _ and give it for free 
---
+++ simple_crud/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class TestMigraty:
+    id: str
+    name: str
+    age: 15
